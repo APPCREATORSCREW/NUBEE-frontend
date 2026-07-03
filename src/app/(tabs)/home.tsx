a184dcf10dbeba2c6f92a4c715e848d879321479
KIM YEEUN <kyee0622@naver.com>
2026-07-03 19:38:00 +0900

    feat: 공통 Button 컴포넌트 추가

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -1,10 +1,23 @@
-import { View, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import Button from '../../components/common/Button';
 
 const Home = () => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>홈</Text>
+    <View style={styles.container}>
+      <Button label="버튼" variant="filled" />
+      <Button label="버튼" variant="outlined" />
+      <Button label="버튼" variant="disabled" />
     </View>
   );
 };
-export default Home
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    gap: 12,
+    paddingHorizontal: 20,
+  },
+});
