6f041c861d90c43931b13996fb548fd32a671bbe
juan6park <juan6park@gmail.com>
2026-07-14 00:22:56 +0900

    feat: 홈 화면 구현 (키워드 육각형 그리드, 방문 상태 zustand 연동)

---
+++ src/components/icons/PolygonPink.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import Svg, { Path } from "react-native-svg";
+import type { SvgProps } from "react-native-svg";
+const SvgPolygonPink = (props: SvgProps) => (
+  <Svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={167}
+    height={144}
+    viewBox="0 0 167 145"
+    fill="none"
+    {...props}
+  >
+    <Path
+      fill="#FDC8E6"
+      d="M0 71.88 41.75 0h83.5L167 71.88l-41.75 71.88h-83.5z"
+    />
+  </Svg>
+);
+export default SvgPolygonPink;
